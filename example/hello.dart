f0190ed3c6b9d1f621196535e6bd8d19f11ce235
aprxty3 <ajiprxty3@gmail.com>
2022-08-27 23:37:25 +0700

    ekse

--- example/hello.dart
+++ example/hello.dart
@@ -1,5 +1,10 @@
 import 'package:create_dart_library/hello.dart';
+import 'package:create_dart_library/src/customer.dart';
 
 void main() {
   print(sayHello('ajjiii'));
+
+  var customer = Customer();
+  var category = Category();
+  var product = Product();
 }
